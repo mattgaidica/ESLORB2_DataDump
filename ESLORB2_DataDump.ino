ea0ab5c816b5f00a5604df57322d6b81d971415d
Matt Gaidica <matt@gaidi.ca>
2021-07-17 08:05:19 -0400

    Update ESLORB2_DataDump.ino

--- ESLORB2_DataDump.ino
+++ ESLORB2_DataDump.ino
@@ -31,9 +31,10 @@ void setup() {
   // Open serial communications and wait for port to open:
   Serial.begin(115200);
   Serial1.begin(115200);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
+  // do not wait, computer is optional
+//  while (!Serial) {
+//    ; // wait for serial port to connect. Needed for native USB port only
+//  }
 
   Serial.print("Initializing SD card...");
 
